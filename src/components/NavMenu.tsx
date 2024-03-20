efcc79646454633b5bf5ce25a2b4965feb7b9cb0
Yegor Kirsanov <easywayroot@gmail.com>
2024-03-20 11:18:57 +0300

    Menu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,12 +1,15 @@
 import { Box, Typography } from "@mui/material"
 import { Link } from "react-router-dom";
 import { HomeIcon } from "../assets/icons/HomeIcon";
+import { PermitsIcon } from "../assets/icons/PemitsIcon";
+import { ServicesIcon } from "../assets/icons/ServicesIcon";
 
 export const NavMenu = () => {
   return <Box sx={{
     width: '100%', 
     p: 2,
     display: 'flex', 
+    direction: 'rtl',
     justifyContent: 'space-between'
   }}>
   <Box>
@@ -14,13 +17,42 @@ export const NavMenu = () => {
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      gap: 2
+      gap: 2,
+      '& svg': {
+        fontSize: 30
+      }
     }}>
       <HomeIcon />
       الرئسية
     </Typography>
   </Box>
-  <Typography to="/" component={Link}>Permits</Typography>
-  <Typography to="/" component={Link}>Service</Typography>
+  <Box>
+    <Typography to="/" component={Link} sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: 1.5,
+      '& svg': {
+        fontSize: 30
+      }
+    }}>
+      <PermitsIcon />
+      التصاريخ
+    </Typography>
+  </Box>
+  <Box>
+    <Typography to="/" component={Link} sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: 2,
+      '& svg': {
+        fontSize: 30
+      }
+    }}>
+      <ServicesIcon />
+      الخدمات
+    </Typography>
+  </Box>
 </Box>
 }
